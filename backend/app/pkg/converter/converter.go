00e832f865af8e7fbd8fa366a2db6ec976641234
Eugeniy Barannikov <e.a.barannikov@outlook.com>
2022-02-01 02:44:40 +0300

    add decTo63 converter

---
+++ backend/app/pkg/converter/converter.go
@@ -0,0 +1,58 @@
+package converter
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+type Converter struct {
+	alphabetCapacity  int64
+	alphabetMap       map[int64]string
+	reversAlphabetMap map[string]int64
+}
+
+func NewConverter(alphabet []byte) *Converter {
+	alphabetCapacity := len(alphabet)
+	alphabetMap := make(map[int64]string, alphabetCapacity)
+	reversAlphabetMap := make(map[string]int64, alphabetCapacity)
+	for id, symbol := range alphabet {
+		idInt64, letter := int64(id), string(symbol)
+		alphabetMap[idInt64], reversAlphabetMap[letter] = letter, idInt64
+	}
+	fmt.Println("capacity", alphabetCapacity)
+	return &Converter{alphabetMap: alphabetMap, reversAlphabetMap: reversAlphabetMap, alphabetCapacity: int64(alphabetCapacity)}
+}
+func (c *Converter) Encode(n int64) string {
+	var out []string
+	for n > c.alphabetCapacity {
+		out = append(out, c.alphabetMap[n%c.alphabetCapacity])
+		n /= c.alphabetCapacity
+	}
+	out = append(out, c.alphabetMap[n])
+
+	out = reverseStringSlice(out)
+	sBuilder := strings.Builder{}
+	for _, letter := range out {
+		sBuilder.WriteString(letter)
+	}
+
+	return sBuilder.String()
+}
+func (c *Converter) Decode(in string) int64 {
+	set := reverseStringSlice(strings.Split(in, ""))
+	var sum int64
+	for i, k := range set {
+		sum += c.reversAlphabetMap[k] * int64(math.Pow(float64(c.alphabetCapacity), float64(i)))
+	}
+	return sum
+}
+func reverseStringSlice(in []string) []string {
+	inLength := len(in)
+	halfLength := inLength / 2
+	inLength--
+	for i := 0; i < halfLength; i++ {
+		in[i], in[inLength-i] = in[inLength-i], in[i]
+	}
+	return in
+}
